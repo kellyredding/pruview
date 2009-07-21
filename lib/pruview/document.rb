5e58956910a5c26ae0b8702c3c037c1c9bb86c63
Kelly <kelly@kelredd.com>
2009-07-21 15:13:48 -0500

    basic port from old plugin version

---
+++ lib/pruview/document.rb
@@ -0,0 +1,154 @@
+module Pruview
+
+  class Document
+    require 'rubygems'
+    
+    # Mini magick wrappers ImageMagick mogrify commands, operating on temporary files instead of in memory
+    gem 'mini_magick'
+    require 'mini_magick'
+    
+    def initialize(source, target_dir)
+      raise "Invalid source file:: #{source.to_s}" if !File.file?(source)
+      raise "Invalid target directory: #{target_dir.to_s}" if !File.directory?(target_dir)
+      raise "Document not supported - file extension: " + file_extension(source) if !format_supported?(source)
+      @source = source
+      @target_dir = target_dir
+      @image = process_image(get_image(source))
+      @tempfile = nil
+    end
+  
+    def to_jpg(name, width, height, crop = false)
+      scale_img = scale_image(width, height, crop)
+      scale_img.combine_options do |img|
+        img.format 'jpg'
+        img.quality '90'
+        img.interlace 'plane'
+      end
+      target = File.join(@target_dir, name.to_s + '.jpg')
+      scale_img.write(target)
+      return target
+    end
+    
+  protected
+  
+    def format_supported?(source)
+      file_ext = file_extension(source)
+      #return true if file_ext == PSD_EXT  # don't support photoshop for now
+      POSTSCRIPT_EXT.each { |extension| return true if file_ext == extension }
+      IMAGE_EXT.each { |extension| return true if file_ext == extension }
+      return false
+    end
+    
+    def format_postscript?(source)
+      file_ext = file_extension(source)
+      POSTSCRIPT_EXT.each { |extension| return true if file_ext == extension }
+      return false
+    end
+    
+    def file_extension(source_file)
+       File.extname(source_file).downcase.chomp  
+    end
+    
+    def get_image(source)
+      source = get_postscript_source(source) if format_postscript?(source)
+      begin
+        return MiniMagick::Image.from_file(source)
+      rescue Exception => err
+        raise "Error reading source image: #{err.message}"
+      end
+    end
+    
+    def get_postscript_source(source)
+      begin
+        @tempfile = MiniMagick::ImageTempFile.new("pruview.jpg")
+        @tempfile.binmode
+      ensure
+        @tempfile.close
+      end
+      run_system_command("convert -format jpg \"#{source}[0]\" \"#{@tempfile.path}\"", "Error processing postscript document")
+      return @tempfile.path
+    end
+    
+    def process_image(image)
+      image.format PROCESS_FORMAT
+      set_RGB_colorspace(image)
+      image.strip
+      return image
+    end
+    
+    def set_RGB_colorspace(image)
+      colorspace = run_system_command("identify -format \"%r\" #{image.path}", "Error reading document colorspace")
+      puts "Colorspace: #{colorspace}"
+      if colorspace =~ /CMYK/
+        image.combine_options do |img|
+          img.profile File.join(File.dirname(__FILE__), 'USWebCoatedSWOP.icc')
+          img.profile File.join(File.dirname(__FILE__), 'sRGB.icm')
+          img.colorspace 'sRGB'
+        end
+      end
+    end
+
+    def scale_image(width, height, crop = false)
+      begin
+        image = MiniMagick::Image.from_file(@image.path)
+        crop_image(image, crop)
+        image.resize "#{width}x#{height}" if crop || @image[:width].to_i > width || @image[:height] > height 
+        return image
+      rescue Exception => err
+        raise "Error scaling image: #{err.message}"
+      end
+    end
+    
+    def crop_image(image, ratio)
+      if ratio.kind_of?(Array) && ratio.length == 2
+        ratio_width, ratio_height = ratio
+        puts "image orig size: #{image[:width].to_i}x#{image[:height].to_i}"
+        puts "ratio_width: #{ratio_width}, ratio_height: #{ratio_height}"
+        if ratio_width > ratio_height || (ratio_width == ratio_height && image[:height].to_i > image[:width].to_i)
+          # calc ratio height from width
+          rheight = (image[:width].to_i*(ratio_height.to_f / ratio_width.to_f)).round
+          puts "rheight: #{rheight}"
+          # shave off height
+          shave_off = ((image[:height].to_i - rheight)/2).round
+          puts "shave off height: #{image[:height].to_i - rheight}"
+          image.shave("0x#{shave_off}")
+          puts "image crop size: #{image[:width].to_i}x#{image[:height].to_i}"
+        elsif ratio_height > ratio_width || (ratio_width == ratio_height && image[:width].to_i > image[:height].to_i)
+          # calc ratio width from height
+          rwidth = (image[:height].to_i*(ratio_width.to_f / ratio_height.to_f)).round
+          # shave off width
+          shave_off = ((image[:width].to_i - rwidth).to_f / 2.to_f).round
+          image.shave("#{shave_off}x0")
+        end
+      end
+      return image
+    end
+    
+    def run_system_command(command, error_message)
+      output = `#{command}`
+      raise "#{error_message}: error given #{$?}\n#{output}" if $? != 0
+      return output
+    end
+    
+  end
+  
+  # Configurations
+  Document::PROCESS_FORMAT = 'jpg'
+  
+  Document::PSD_EXT = '.psd'
+  Document::POSTSCRIPT_EXT = [
+    '.pdf',
+    '.eps',
+    '.ai'
+  ]
+  Document::IMAGE_EXT = [
+    '.bmp',
+    '.gif',
+    '.jpg',
+    '.jpeg',
+    '.png',
+    '.tga'
+  ]
+
+end
+
---
+++ lib/pruview/video.rb
@@ -0,0 +1,180 @@
+module Pruview
+
+  class Video
+  
+    require 'yaml'
+    
+    # this class assumes you have 'ffmpeg' and 'flvtool2' installed and in your path
+
+    def initialize(source, target_dir, bitrate_mult = 1)
+      raise "Invalid source file:: #{source.to_s}" if !File.file?(source)
+      raise "Invalid target directory: #{target_dir.to_s}" if !File.directory?(target_dir)
+      raise "Document not supported - file extension: " + file_extension(source) if !format_supported?(source)
+      @source = source
+      @target_dir = target_dir
+      @bitrate_multiplier = bitrate_mult
+    end
+  
+    def to_flv(name, width, height, scale_static = false)
+      target = to_base(name, width, height, '.flv', scale_static)
+      run("#{FLVTOOL} -U #{target}", "Unable to add scrubbing meta-data for #{target}.")
+      return target
+    end
+    
+    def to_mov(name, width, height, scale_static = false)
+      target = to_base(name, width, height, '.mov', scale_static)
+      # run qt-faststart
+      return target
+    end
+    
+  protected
+  
+    def to_base(name, width, height, extension, scale_static)
+      target = File.join(@target_dir, name.to_s + extension)
+      info_yml = File.join(@target_dir, name.to_s + '_info.yml')
+      run(build_command(@source, target, width, height, get_info(info_yml), scale_static), "Unable to convert #{@source} to #{target}.")
+      return target
+    end
+  
+    def format_supported?(source)
+      file_ext = file_extension(source)
+      EXT.each { |extension| return true if file_ext == extension }
+      return false
+    end
+    
+    def file_extension(source_file)
+       File.extname(source_file).downcase.chomp  
+    end
+    
+    def get_info(yml)
+      run("#{FFYML} #{@source} #{yml}", "Unable to get video info")
+      YAML.load_file(yml)
+    end
+  
+    def build_command(source, target, width, height, info, scale_static)
+      command = "#{FFMPEG} -i #{source}"
+      command += get_scale_command(info['width'], info['height'], width, height, scale_static)
+      scale_factor = get_scale_factor(info['width'], info['height'], width, height)
+      if file_extension(target) != '.flv' # use h264 codec with lower bitrate scaling factor
+        command += " -vcodec h264" 
+        scale_factor /= 2.0
+      end
+      puts "scale factor: #{scale_factor.to_s}"
+      if !info['bitrate'].zero?
+        command += " -b #{(info['bitrate']*@bitrate_multiplier*scale_factor).to_s}"
+      else
+        command += " -sameq"
+      end
+      command += " -ab #{AUDIO_BITRATE}"
+      command += " -ar #{AUDIO_SAMPLING}"
+      command += " -y #{target}"
+    end
+    
+    def get_scale_factor(source_width, source_height, scale_width, scale_height)
+      if source_width > source_height
+        return (scale_width.to_f / source_width.to_f)
+      else
+        return (scale_height.to_f / source_height.to_f)
+      end
+    end
+    
+    def get_scale_command(source_width, source_height, scale_width, scale_height, static)
+      # this type of scaling assumes a static overall resolution with black padding added appropriately
+      # to keep the meaningful video area at the source aspect ratio
+      scale_params = get_scaling_params(source_width, source_height, scale_width, scale_height)
+      scale_command = " -s #{scale_params[:width].to_s}x#{scale_params[:height].to_s}"
+      if static == true
+        scale_command += " -padcolor #{PAD_COLOR}"
+        scale_command += " -padleft #{scale_params[:left]}" if scale_params[:left] > 0
+        scale_command += " -padtop #{scale_params[:top]}" if scale_params[:top] > 0
+        scale_command += " -padright #{scale_params[:right]}" if scale_params[:right] > 0
+        scale_command += " -padbottom #{scale_params[:bottom]}" if scale_params[:bottom] > 0
+      end
+      scale_command
+    end
+    
+    def get_scaling_params(source_width, source_height, scale_width, scale_height)
+      params = {}
+      params[:left],params[:top],params[:right],params[:bottom] = 0,0,0,0
+      params[:width],params[:height] = scale_width, scale_height
+      scale_aspect = scale_width.to_f / scale_height.to_f
+      source_aspect = source_width.to_f / source_height.to_f
+      if(source_aspect > scale_aspect)
+        params[:height] = (scale_width.to_f / source_aspect).to_i
+        params[:height] += 1 if params[:height] % 2 != 0
+        params[:top] = params[:bottom] = ((scale_height - params[:height]).to_f / 2).to_i
+        params[:top] = params[:bottom] += 1 if params[:top] % 2 != 0
+      elsif(source_aspect < scale_aspect)
+        params[:width] = (scale_height.to_f * source_aspect).to_i
+        params[:width] += 1 if params[:width] % 2 != 0
+        params[:left] = params[:right] = ((scale_width - params[:width]).to_f / 2).to_i
+        params[:left] = params[:right] += 1 if params[:left] % 2 != 0
+      end
+      return params
+    end
+
+    def run(command, error_message = "Unknown error.")
+      raise "Ffmpeg error: " + error_message + " - command: '#{command}'" if !system(command)
+    end
+  
+  # Configurations
+  Video::FFYML = File.join(File.dirname(__FILE__), '..','bin','ffyml')
+  Video::FFMPEG = 'ffmpeg'
+  Video::FLVTOOL = 'flvtool2'
+  Video::PAD_COLOR = "000000"
+  Video::AUDIO_BITRATE = '128' # kbps
+  Video::AUDIO_SAMPLING = '44100'
+  
+  Video::EXT = [
+   '.avi',
+   '.flv',
+   '.mov',
+   '.mpg',
+   '.mp4'
+  ]
+
+  end
+  
+  
+  
+  class VideoImage
+  
+    # this class assumes you have 'ffmpeg' installed and in your path
+
+    def self.to_jpg(source, target_dir, name)
+      raise "Invalid source file:: #{source.to_s}" if !File.file?(source)
+      raise "Invalid target directory: #{target_dir.to_s}" if !File.directory?(target_dir)
+      raise "Document not supported - file extension: " + file_extension(source) if !format_supported?(source)
+      target = File.join(target_dir, name.to_s + '.jpg')
+      run(build_command(source, '-ss 00:00:00.2', 'mjpeg', target), "Unable to get preview image for #{target}")
+      # analyze image - create better
+      return target
+    end
+    
+  protected
+    
+    def self.format_supported?(source)
+      file_ext = file_extension(source)
+      Video::EXT.each { |extension| return true if file_ext == extension }
+      return false
+    end
+    
+    def self.file_extension(source_file)
+       File.extname(source_file).downcase.chomp  
+    end
+    
+    def self.build_command(source, time_str, format, target)
+      command = "#{Video::FFMPEG} -i #{source}"
+      command += " #{time_str}"
+      command += " -f #{format}" if !format.empty?
+      command += " -an -y #{target}"
+    end
+    
+    def self.run(command, error_message = "Unknown error.")
+      raise "Ffmpeg error: " + error_message + " - command: '#{command}'" if !system(command)
+    end
+  
+  end
+
+end
+
